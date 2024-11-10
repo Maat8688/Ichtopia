67085dc86fd88dc3df9b2149b2bb1ad547071ecd
Roemsi12 <149324225+Roemsi12@users.noreply.github.com>
2024-11-10 19:28:03 +0200

    blub

--- app/static/js/socketio.js
+++ app/static/js/socketio.js
@@ -1,13 +1,13 @@
 const socket = io();
 
-socket.on('connect', () => {
-    console.log('Connected to server');
-    socket.send('Hello, Server!');
+socket.on("connect", () => {
+  console.log("Connected to server");
+  socket.send("Hello, Server!");
 });
 
-socket.on('message', (data) => {
-    console.log(data);
-    const messageElement = document.createElement('div');
-    messageElement.textContent = data;
-    document.getElementById('messages').appendChild(messageElement);
+socket.on("message", (data) => {
+  console.log(data);
+  const messageElement = document.createElement("div");
+  messageElement.textContent = data;
+  document.getElementById("messages").appendChild(messageElement);
 });
